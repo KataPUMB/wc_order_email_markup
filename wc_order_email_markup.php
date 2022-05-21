283e3bfcfcfdd9de100b9aa613857b2956ed3469
KataPUMB <kaylakmail@gmail.com>
2022-05-21 16:41:28 +0200

    Instantiated

---
+++ wc_order_email_markup.php
@@ -0,0 +1,10 @@
+<?php
+/**
+Plugin Name: WooCommerce Order Email Markup
+Plugin URI: http://wordpress.org/plugins/wc-order-email-markup
+Description: Automatcially add email markup schema to your orders emails so it improves your relation with your client and build trust for your website
+Author: Juan Martínez Alonso
+Version: 1.0.0
+Author URI: https://internaftis.com/
+*/
+?>
